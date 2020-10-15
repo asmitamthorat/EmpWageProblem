9bcc18940e53c199071fa0e04b1d3dfc4ccc784a
asmitamthorat <asmita26296@gmail.com>
2020-10-15 19:40:59 +0530

    calculating daily wage

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,9 +1,13 @@
 public class EmpWageCalculation {
     public static void main(String args[]){
+        int wagePerHr=20;
         int isEmpPresent=1;
+        int hrForDay=8;
+        int empWage;
         double empCheck=Math.floor(Math.random()*10)%2;
         if(empCheck==isEmpPresent){
-            System.out.println("employee is present");
+            empWage=wagePerHr*hrForDay;
+            System.out.println("employee wage :"+empWage);
         }
         else{
             System.out.println("employee is absent");
