ea8df153ae4872dc1da16615569232f3a3cfcd1d
asmitamthorat <asmita26296@gmail.com>
2020-10-15 19:28:21 +0530

    checking for employee presence

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,6 +1,12 @@
 public class EmpWageCalculation {
     public static void main(String args[]){
-        System.out.println(" emp wage calculation");
-
+        int isEmpPresent=1;
+        double empCheck=Math.floor(Math.random()*10)%2;
+        if(empCheck==isEmpPresent){
+            System.out.println("employee is present");
+        }
+        else{
+            System.out.println("employee is absent");
+        }
     }
 }
