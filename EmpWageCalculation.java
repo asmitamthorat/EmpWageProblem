7365fa5c742a9841db1b8fe8e69a17c566f76f3d
asmitamthorat <asmita26296@gmail.com>
2020-10-16 08:49:42 +0530

    calculating wages till number working hours per month is reached

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -2,11 +2,15 @@ public class EmpWageCalculation {
     public static final int partTimeEmp=1;
     public static final int fullTimeEmp=2;
     public static final int numberOFWroking_days=5;
+    public static final int max_hrs_perMonth=5;
     public static void main(String args[]) {
         int wagePerHr = 20;
         int empHrs = 0;
         int empWage = 0;
         int totalWage=0;
+        int totalWorkingDays=0;
+        int totalEmpHrs=0;
+        while(totalEmpHrs<=max_hrs_perMonth && totalWorkingDays<numberOFWroking_days)
         for (int day = 0; day < numberOFWroking_days; day++) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -19,10 +23,9 @@ public class EmpWageCalculation {
                 default:
                     empHrs = 0;
             }
-            empWage = wagePerHr * empHrs;
-            totalWage=totalWage+empWage;
-            System.out.println("employee wage :" + empWage);
+            totalEmpHrs=totalEmpHrs+empHrs;
         }
+        totalWage = wagePerHr * totalEmpHrs;
         System.out.println("total wage:"+totalWage );
     }
 }
