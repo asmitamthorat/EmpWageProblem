c8c809975091be9f29bed96f729069e1caca3b73
asmitamthorat <asmita26296@gmail.com>
2020-10-16 08:20:39 +0530

    calcuting employee wage using switch

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,21 +1,21 @@
 public class EmpWageCalculation {
+    public static final int partTimeEmp=1;
+    public static final int fullTimeEmp=2;
     public static void main(String args[]){
         int wagePerHr=20;
-        int partTimeEmp=1;
-        int fullTimeEmp=2;
         int empHrs=0;
         int empWage=0;
-        double empCheck=Math.floor(Math.random()*10)%3;
-        if(empCheck==partTimeEmp){
-           empHrs=4;
+        int empCheck=(int)Math.floor(Math.random()*10)%3;
+        switch (empCheck){
+            case partTimeEmp:
+                empHrs=4;
+                break;
+            case fullTimeEmp:
+                empHrs=8;
+                break;
+            default:
+                empHrs=0;
         }
-        else if (empCheck==fullTimeEmp){
-            empHrs=8;
-        }
-        else{
-            empHrs=0;
-        }
-
         empWage=wagePerHr*empHrs;
         System.out.println("employee wage :"+empWage);
     }
